284bba8e35573b46f48c5856c101b950d9699578
Leon <fanadn2019@gmail.com>
2026-01-12 16:44:49 +0700

    Login Logout Register

---
+++ src/main/java/dao/UserDAO.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.Date;
+import model.User;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class UserDAO {
+
+    public static User checkLogin(User user) {
+        User res = null;
+        try {
+            Connection con = DBConnect.getConnection();
+
+            String sql = "select * from [Users] where phone = ? and password_hash = ?";
+
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, user.getPhone());
+            pst.setString(2, user.getPassword());
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                String fullname = rs.getString("full_name");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                String password = rs.getString("password_hash");
+                int role = Integer.parseInt(rs.getString("role_id"));
+
+                res = new User(fullname, email, phone, password, role, new Date());
+            }
+        } catch (Exception e) {
+        }
+        return res;
+    }
+
+    public static void insert(User user) {
+        try {
+            Connection con = DBConnect.getConnection();
+            String sql = "INSERT INTO [Users](full_name, email, phone, password_hash, role_id, created_at) VALUES(?,?,?,?,?,?)";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, user.getFullname());
+            pst.setString(2, user.getEmail());
+            pst.setString(3, user.getPhone());
+            pst.setString(4, user.getPassword());
+            pst.setInt(5, user.getRole());
+            LocalDate date = LocalDate.now();
+            pst.setDate(6, java.sql.Date.valueOf(date));
+            pst.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static boolean checkPhoneNumber(String phoneNumber) {
+        boolean check = false;
+        try {
+            Connection con = DBConnect.getConnection();
+            String sql = "SELECT *  FROM [users] WHERE phone = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, phoneNumber);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                check = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return check;
+    }
+}
--- target/LoginWeb-1.0-SNAPSHOT/views/user/booking.jsp
+++ target/LoginWeb-1.0-SNAPSHOT/views/user/booking.jsp
@@ -1,17 +1,24 @@
 <%-- Document : booking Created on : Jan 11, 2026, 4:46:03 PM Author : LENOVO --%>
 
-    <%@page contentType="text/html" pageEncoding="UTF-8" %>
-        <!DOCTYPE html>
-        <html>
-        <%-- Trang đặt vé - chọn ghế --%>
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+    <%-- Trang đặt vé - chọn ghế --%>
 
-            <head>
-                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-                <title>JSP Page</title>
-            </head>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+              crossorigin="anonymous"><!-- comment -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+    </head>
 
-            <body>
-                <h1>Trang đặt vé - chọn ghế</h1>
-            </body>
+    <body>
+        <jsp:include page="../../layout/header.jsp"></jsp:include>
+        <h1>Trang đặt vé - chọn ghế</h1>
+    </body>
 
-        </html>
+</html>
